118e9ed92ad2760ff834b98fa8c3f8a09d3bda37
Dave Tromp <davetromp@gmail.com>
2019-08-25 15:48:29 +0200

    First and maybe last commmit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# CoinbaseProBTCBuyingBot
-Script that will buy Bitcoin as soon as there is Fiat balance available
+# CoinbaseProCryptoBuyingBot
+Script that will buy Bitcoin or any other Crypto as soon as there is a Fiat balance available.
+
+##Installation
+Make sure pip and python3 is installed.
+Install the needed requirements by doing:
+```
+pip install -r requirements.txt
+```
+
+##Configuration
+Copy config.ini.example to config.ini.
+Edit the config.ini file with your own API details, fiat and crypto currencies.
+The configuration is set so the script will check every hour (3600 seconds).
+Fee is set to 0.2% so there is always enough left to cover the fees.
+
+##Running the script
+Simply run:
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+from time import sleep
+import configparser
+import ccxt
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+secret = config['coinbasepro']['secret']
+api_key = config['coinbasepro']['api_key']
+password = config['coinbasepro']['password']
+fiat = config['coinbasepro']['fiat']
+crypto = config['coinbasepro']['crypto']
+ticker = '{}/{}'.format(crypto, fiat)
+fee = float(config['coinbasepro']['fee'])
+delay = int(config['coinbasepro']['delay'])
+
+coinbasepro = ccxt.coinbasepro({
+    'apiKey': api_key,
+    'secret': secret,
+    'password': password
+})
+
+def main():
+    try:
+        while True:
+            balance = float(coinbasepro.fetch_balance()[fiat]['free'])
+            print("Current fiat ({}) balance is {}.".format(fiat, balance))
+            if balance > 0:
+                rate = float(coinbasepro.fetch_ticker(ticker)['ask'])
+                ordersize = (balance / rate) * (1.0 - fee)
+                print("Placing a market buy order.\nOrdersize: {}\nTicker: {}\nCurrent price: {}".format(ordersize, ticker, rate))
+                try:
+                    coinbasepro.create_market_buy_order(ticker, ordersize)
+                except Exception as e:
+                    print(e)
+            sleep(delay)
+    except KeyboardInterrupt:
+        print("\nCancelled by user")
+
+
+if __name__ == '__main__':
+    main()
