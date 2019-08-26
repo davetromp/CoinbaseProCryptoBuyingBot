3568c736b198769e0f37e395aa0105d86231cf6f
Dave Tromp <davetromp@gmail.com>
2019-08-26 21:20:47 +0200

    Adding automatic withdraw option

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CoinbaseProCryptoBuyingBot
-Script that will buy Bitcoin or any other Crypto on Coinbase Pro as soon as there is a Fiat balance available.
+This script will buy Bitcoin or any other Crypto on Coinbase Pro as soon as there is a Fiat balance available.
 
 ## Installation
 Make sure pip and python3 is installed.
@@ -13,12 +13,17 @@ Copy config.ini.example to config.ini.
 ```
 cp config.ini.example config.ini
 ```
-Edit the config.ini file with your own API details, fiat and crypto currencies.
+Edit the config.ini file with your own API details, fiat currency and cryptocurrency.
 ```
 nano config.ini
 ```
 The configuration is set so the script will check every hour (3600 seconds).
-Fee is set to 0.2% so there is always enough left to cover the fees.
+Fee is set to 0.25% so there is always enough left to cover the fees, but you can change it as you see fit.
+
+### Withdraw option
+There is the option to automatically withdraw to a wallet address once your crypto holdings reach a certain threshold. By default this option is switched off. You can switch it on by setting withdraw to True.
+Also be sure to set the threshold. Default is 0.0.
+Be sure to have transfers enabled in the permission settings of your API.
 
 ## Running the script
 Simply run:
--- main.py
+++ main.py
@@ -16,6 +16,9 @@ crypto = config['coinbasepro']['crypto']
 ticker = '{}/{}'.format(crypto, fiat)
 fee = float(config['coinbasepro']['fee'])
 delay = int(config['coinbasepro']['delay'])
+withdraw = bool(config['coinbasepro']['withdraw'])
+withdrawThreshold = float(config['coinbasepro']['withdrawThreshold'])
+toAddress = config['coinbasepro']['toAddress']
 
 coinbasepro = ccxt.coinbasepro({
     'apiKey': api_key,
@@ -26,19 +29,23 @@ coinbasepro = ccxt.coinbasepro({
 def main():
     try:
         while True:
-            balance = float(coinbasepro.fetch_balance()[fiat]['free'])
+            fiat_balance = float(coinbasepro.fetch_balance()[fiat]['free'])
             print("#####################")
             print(datetime.datetime.now())
-            print("Current fiat ({}) balance is {}.".format(fiat, balance))
-            if balance > 0:
+            print("Current fiat ({}) balance is {}.".format(fiat, fiat_balance))
+            if fiat_balance > 0:
                 rate = float(coinbasepro.fetch_ticker(ticker)['ask'])
-                ordersize = (balance / rate) * (1.0 - fee)
+                ordersize = (fiat_balance / rate) * (1.0 - fee)
                 print("Placing a market buy order.\nOrdersize: {}\nTicker: {}\nCurrent price: {}".format(ordersize, ticker, rate))
                 try:
                     coinbasepro.create_market_buy_order(ticker, ordersize)
                 except Exception as e:
                     print(e)
-                print("#####################")
+            crypto_balance = float(coinbasepro.fetch_balance()[crypto]['free'])
+            if crypto_balance > withdrawThreshold and withdraw == True:
+                print("Sending {} of {} to {}.".format(crypto_balance, crypto, toAddress))
+                coinbasepro.withdraw(crypto, crypto_balance, toAddress, tag=None, params={})
+            print("#####################")
             sleep(delay)
     except KeyboardInterrupt:
         print("\nCancelled by user")
